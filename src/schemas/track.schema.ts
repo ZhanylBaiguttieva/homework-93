620217e78312fda3f9136e12bdacb2ee5f2f99d2
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-03-21 14:43:39 +0600

    Added create-track.dto.ts,track-schema.ts,tracks.controller.ts

---
+++ src/schemas/track.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+import { Album } from "./album.schema";
+
+@Schema()
+export class Track {
+  @Prop({ref: Album.name, required: true})
+  album: mongoose.Schema.Types.ObjectId;
+
+  @Prop({required: true, unique: true})
+  name: string;
+
+  @Prop()
+  length: string;
+
+  @Prop({required: true, min: 0})
+  number: number;
+}
+
+export const TrackSchema = SchemaFactory.createForClass(Track);
+
+export type TrackDocument = Track & Document;
---
+++ src/tracks/tracks.controller.ts
@@ -0,0 +1,71 @@
+import {
+  Body,
+  Controller, Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Query,
+} from "@nestjs/common";
+
+import { Model } from "mongoose";
+import { InjectModel } from "@nestjs/mongoose";
+import { Track, TrackDocument } from "../schemas/track.schema";
+import { CreateTrackDto } from "./create-track.dto";
+
+@Controller('tracks')
+export class TracksController {
+  constructor(
+    @InjectModel(Track.name)
+    private trackModel: Model<TrackDocument>,
+  ) {}
+
+
+  @Get()
+  getAll(@Query() params: any) {
+    if(params.album) {
+      return this.trackModel
+        .find({album: params.album})
+        .sort({ number: 'asc' })
+        .populate('album', 'name date');
+    }
+    return this.trackModel
+      .find()
+      .sort({ number: 'asc' })
+      .populate('album', 'name date');
+  }
+
+  @Get('/:id')
+  async getOne(@Param('id') id:string){
+    const track = await this.trackModel
+      .findById(id)
+      .populate('album', 'name date');
+
+    if (!track) {
+      throw new NotFoundException('No such track');
+    }
+    return track;
+  }
+
+  @Post()
+  create(
+
+    @Body() trackData: CreateTrackDto
+  ) {
+    const track = new this.trackModel({
+      album: trackData.album,
+      name: trackData.name,
+      length: trackData.length,
+      number: trackData.number,
+    });
+    return track.save();
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id') id:string) {
+    await this.trackModel.findByIdAndDelete(id);
+    return {message: 'This track was deleted'}
+  }
+}
+
+
