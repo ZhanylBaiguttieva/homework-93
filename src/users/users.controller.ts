25f49eed98656258a082a445d44873133af50060
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-03-25 17:56:32 +0600

    HW#94 - Added logout in users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, Req, UnprocessableEntityException, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Post, Req, UnprocessableEntityException, UseGuards } from "@nestjs/common";
 import mongoose, { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { User, UserDocument } from "../schemas/user.schema";
 import { RegisterUserDto } from "./register-user.dto";
 import { AuthGuard } from "@nestjs/passport";
 import { Request } from "express";
+import { TokenAuthGuard } from "../auth/token-auth.guard";
 
 
 @Controller('users')
@@ -33,9 +34,27 @@ export class UsersController {
   }
 
   @UseGuards(AuthGuard('local'))
-  @Post('/sessions')
+  @Post('sessions')
   async login(@Req() req: Request) {
     return { message: 'Correct', user: req.user };
   }
 
+  @UseGuards(TokenAuthGuard)
+  @Delete('sessions')
+  async logout(@Req() req: Request){
+    const headerValue = req.get('Authorization');
+    if (!headerValue) return true;
+
+    const [_bearer, token] = headerValue.split(' ');
+    if (!token) return true;
+
+    const user = await this.userModel.findOne({ token });
+
+    if (!user) {
+      return true;
+    }
+    user.generateToken();
+    await user.save();
+    return { message: 'Success' };
+  }
 }
